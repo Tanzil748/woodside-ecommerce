5a0c18e9a9fc6ccbe4a52872149e1bb680094f88
Tanzil Hassan <tanzilhassan333@gmail.com>
2023-01-20 23:40:11 -0500

    Updated cart button design

--- frontend/src/components/NavComponent.jsx
+++ frontend/src/components/NavComponent.jsx
@@ -3,6 +3,7 @@ import "../styles/navbar.css";
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
+import { BsCart4 } from "react-icons/bs";
 
 const NavComponent = () => {
   return (
@@ -15,13 +16,23 @@ const NavComponent = () => {
           <Navbar.Toggle aria-controls="navbarScroll" />
           <Navbar.Collapse id="navbarScroll">
             <Nav
-              className="me-auto my-2 my-lg-0"
+              className="me-auto my-2 my-lg-0 d-flex align-items-center"
               style={{ maxHeight: "100px" }}
               navbarScroll
             >
               <Nav.Link href="/signin">Sign In</Nav.Link>
               <Nav.Link href="/register">Register</Nav.Link>
-              <Nav.Link href="/cart">Cart</Nav.Link>
+              <Nav.Link href="/cart">
+                <button
+                  type="button"
+                  class="btn btn-secondary position-relative"
+                >
+                  <BsCart4 size={20} />
+                  <span class="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger">
+                    10
+                  </span>
+                </button>
+              </Nav.Link>
             </Nav>
           </Navbar.Collapse>
         </div>
