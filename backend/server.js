c925519b05708d3e43eb94c82c6f555c29cadc99
Tanzil Hassan <tanzilhassan333@gmail.com>
2023-01-27 21:33:07 -0500

    Installed concurrently

--- backend/server.js
+++ backend/server.js
@@ -4,5 +4,8 @@ require("dotenv").config();
 const port = process.env.PORT || 5500;
 
 app.use(express.json());
+app.get("/", (req, res) => {
+  res.send("Hello backend");
+});
 
 app.listen(port, () => console.log(`Backend running on port ${port}`));
